6893cd099306cc9a7c396862a8c2c5059bfef084
Mittal-Malankiya <m4mittal19@gmail.com>
2024-03-25 12:21:14 +0100

    update package.json

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,11 +2,11 @@ import { render } from "@testing-library/react";
 import App from "../App";
 
 describe("<App /> component", () => {
-  let AppDome;
+  let AppDOM;
   beforeEach(() => {
-    AppDome = render(<App />).container.firstChild;
+    AppDOM = render(<App />).container.firstChild;
   });
-  test("renders list of events", () => {
-    expect(AppDome.querySelector("#event-list")).toBeInTheDocument();
+  test("renders EventList", () => {
+    expect(AppDOM.querySelector("#event-list")).toBeInTheDocument();
   });
 });
