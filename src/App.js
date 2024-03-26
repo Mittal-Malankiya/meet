90d583e961fff9881ef5534ce041fd08074a1d33
Mittal-Malankiya <m4mittal19@gmail.com>
2024-03-26 11:42:18 +0100

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
+      <CitySearch />
       <EventList />
     </div>
   );
