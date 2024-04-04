30db4c9deffded0ffa45c12b7f1d22c46954ecf9
Mittal-Malankiya <m4mittal19@gmail.com>
2024-04-04 11:26:16 +0200

    update app.js

--- src/App.js
+++ src/App.js
@@ -55,7 +55,9 @@ const App = () => {
         setCurrentNOE={setCurrentNOE}
         setErrorAlert={setErrorAlert}
       />
-      <CityEventsChart allLocations={allLocations} events={events} />
+      <div className="charts-container">
+        <CityEventsChart allLocations={allLocations} events={events} />
+      </div>
       <EventList events={events} />
     </div>
   );
